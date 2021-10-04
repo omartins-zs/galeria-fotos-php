60580f0a6618b2ff82ab18ebe7f5ea7c59ed77d0
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-04 17:31:29 -0300

    Refatorando e Redirecionamento Loop Infinito

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <body>
     <?php
 
-  $imagens = array('./img/japan.jpg', './img/japan_street.jpg', './img/buildings.jpg');
+  $imagens = array('./img/japan.jpg', './img/buildings.jpg', './img/japan_street.jpg');
 
   $indice = 0;
 
@@ -26,17 +26,22 @@
   } else {
     $indice = (int)$_GET["foto"];
 
-    $anteriorImagem = $indice - 1;
-    $proximaImagem = $indice + 1;
+    $anteriorImagem = (($indice - 1) < 0) ? count($imagens) - 1 : ($indice - 1);
+    $proximaImagem = (($indice + 1) >= count($imagens)) ? 0 : ($indice + 1);;
 
     if ($indice < 0) {
       $indice = 0;
+      echo '<script>window.location.href="index.php?foto='.$indice.'"</script>';
+      die();
+      
       $anteriorImagem = count($imagens) - 1;
       $proximaImagem = 1;
     }
 
     if ($indice >= count($imagens)) {
       $indice = count($imagens) - 1;
+      echo '<script>window.location.href="index.php?foto='.$indice.'"</script>';
+      die();
 
       $anteriorImagem = count($imagens) - 2;
       $proximaImagem = 0;
